1ce769254d5088c0af774fc1961eb046988bd813
Pierre-Olivier BAUDINO <baudinopierreolivier@gmail.com>
2022-05-05 15:54:47 +0200

    initiation db and add it to gitignore

--- shop/shop/settings.py
+++ shop/shop/settings.py
@@ -76,8 +76,15 @@ WSGI_APPLICATION = 'shop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'c58baudino',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 
--- shop/store/models.py
+++ shop/store/models.py
@@ -1,3 +1,6 @@
 from django.db import models
+from django.db import connections
 
 # Create your models here.
+class Product(models.Model):
+    name = models.Charfield(max_length=128)
