fba8695fe6683351fba6e0520df5082947039493
Pierre-Olivier BAUDINO <baudinopierreolivier@gmail.com>
2022-05-06 09:54:22 +0200

    add image on product page

--- shop/store/views.py
+++ shop/store/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+from store.models import Product
+
+def index(request):
+    products = Product.objects.all()
+    return render(request, 'store/index.html', context={"products": products})
