b4376352235f305282e73a845c84925d8c84bb49
Pierre-Olivier BAUDINO <baudinopierreolivier@gmail.com>
2022-05-06 15:31:49 +0200

    add cart action and verifications of being

--- shop/shop/urls.py
+++ shop/shop/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path
 
 from accounts.views import signup, logout_user, login_user
-from store.views import index, product_detail
+from store.views import index, product_detail, add_to_cart, cart
 
 from shop import settings
 
@@ -28,6 +28,8 @@ urlpatterns = [
     path('signup/', signup, name='signup'),
     path('login/', login_user, name='login'),
     path('logout/', logout_user, name='logout'),
+    path('cart/', cart, name='cart'),
     path('product/<str:slug>/', product_detail, name='product'),
+    path('product/<str:slug>/add-to-cart', add_to_cart, name='add-to-cart'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) #version debug = true dif version false
 
--- shop/store/views.py
+++ shop/store/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.urls import reverse
 
-from store.models import Product
+from store.models import Product, Cart, Order
 
 
 def index(request):
@@ -12,3 +13,23 @@ def index(request):
 def product_detail(request, slug):
     product = get_object_or_404(Product, slug=slug)
     return render(request, 'store/detail.html', context={"product": product})
+
+
+#video 2h00/2h05 attention on est dans le vue il faut faire le if dans model
+def add_to_cart(request, slug):
+    user = request.user
+    product = get_object_or_404(Product, slug=slug)
+    cart, _ = Cart.objects.get_or_create(user=user)
+    order, created = Order.objects.get_or_create(user=user, product=product)
+
+    if created:
+        cart.orders.add(order)
+        cart.save()
+    else:
+        order.quantity += 1
+        order.save()
+
+    return redirect(reverse("product", kwargs={"slug": slug}))
+
+def cart(request):
+    pass
