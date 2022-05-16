733e0b3883d0a8146f1ac15261b2c90999474bad
Pierre-Olivier BAUDINO <baudinopierreolivier@gmail.com>
2022-05-16 22:50:39 +0200

    add real db and first try combination

--- shop/store/admin.py
+++ shop/store/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
-from store.models import Product, Order, Cart
+#from store.models import Product, Order, Cart
 #from store.models import City, User, Cart
-
+from store.models import Product
 
 admin.site.register(Product)
-admin.site.register(Order)
-admin.site.register(Cart)
+#admin.site.register(Order)
+#admin.site.register(Cart)
 
 
 """
--- shop/store/views.py
+++ shop/store/views.py
@@ -2,9 +2,12 @@ from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse
 
-from store.models import Product, Cart, Order
+#from store.models import Product, Cart, Order
 
+from store.models import Recipe
+#from store.models import City, User, Product, Category, Recipe, Image, #Measurement, Ingredients, Article, ArticlePrice, Lot, Command, UserLog, Chief, Moderator, Admin, Commentary, Paragraph, RecipeIngredient, Importation, OrderLine, GivesGrade
 
+"""
 def index(request):
     products = Product.objects.all()
     return render(request, 'store/index.html', context={"products": products})
@@ -41,3 +44,12 @@ def delete_cart(request):
     if cart := request.user.cart: #concatenation assignation et condition
         cart.delete()
     return redirect('index')
+"""
+#section test:
+
+
+def index(request):
+    recipes = Recipe.objects.all()
+    return render(request, 'store/index.html', context={"recipes": recipes})
+
+
